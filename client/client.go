6abfa84ef9de8605def47f5d96b3375df2b6b127
Ethan Miller <ethanmillerindy@gmail.com>
2020-04-30 19:36:23 -0400

    move client stuff out

--- client/client.go
+++ client/client.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"context"
-	"io"
 	"log"
 
+	"github.com/ethanmil/bolo/client/bologame"
 	"github.com/ethanmil/bolo/client/bullet"
 	"github.com/ethanmil/bolo/client/maps"
 	"github.com/ethanmil/bolo/client/tank"
@@ -34,7 +34,7 @@ func init() {
 }
 
 func main() {
-	bolo := NewBolo()
+	bolo := bologame.New(0, art, bullet.NewManager(art))
 
 	// create client
 	opts := []grpc.DialOption{
@@ -71,58 +71,4 @@ func main() {
 	if err != nil {
 		logrus.Fatal(err)
 	}
-}
-
-// Bolo -
-type Bolo struct {
-	id            int32
-	world         *maps.WorldMap
-	tanks         []tank.Tank
-	bulletManager *bullet.Manager
-	client        guide.BoloClient
-	tankStreamOut guide.Bolo_SendTankDataClient
-}
-
-// NewBolo -
-func NewBolo() *Bolo {
-	bulletManager := bullet.NewManager(art)
-
-	return &Bolo{
-		id:            0,
-		bulletManager: bulletManager,
-	}
-}
-
-// Update -
-func (b *Bolo) Update(screen *ebiten.Image) error {
-	b.world.Draw(screen)
-
-	for i := range b.tanks {
-		b.tanks[i].Update(2)
-		b.tanks[i].Draw(screen)
-	}
-
-	b.bulletManager.Update(2)
-	b.bulletManager.Draw(screen)
-
-	err = b.tankStreamOut.Send(&guide.Tank{
-		Id: b.id,
-		X:  float32(b.tanks[b.id].Element.Position.X),
-		Y:  float32(b.tanks[b.id].Element.Position.Y),
-	})
-	if err != nil && err != io.EOF {
-		log.Fatalf("Send: %v", err)
-	}
-
-	return nil
-}
-
-// Draw -
-func (b *Bolo) Draw(screen *ebiten.Image) error {
-	return nil
-}
-
-// Layout -
-func (b *Bolo) Layout(width, height int) (int, int) {
-	return 800, 600
 }
